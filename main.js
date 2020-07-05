fcf4aec1f9b58407612cbb795b15e7040d28b91e
Dibya <dmallick@salesforce.com>
2020-07-06 01:44:23 +0530

    draft

---
+++ main.js
@@ -0,0 +1,10 @@
+var foo = require("./modules/module1.js")
+var bar1 = require("./modules/module2.js").bar1
+var bar2 = require("./modules/module2.js").bar2
+
+
+foo();
+bar1();
+bar2();
+
+
---
+++ modules/module2.js
@@ -0,0 +1,11 @@
+function bar1() {
+	console.log("bar1");
+}
+
+function bar2() {
+	console.log("bar2");
+}
+
+exports.bar1 = bar1
+exports.bar2 = bar2
+
